ce6fab99e8b0ae4fbcd8b5ca55aab4c3d564a81b
Tiago Soriano <tiagosoriano2000@gmail.com>
2020-02-18 23:48:16 -0500

    Added the checkAnswer function

--- game.js
+++ game.js
@@ -4,54 +4,63 @@ $(document).ready(function() {
     var buttonColours = ["red", "green", "blue", "yellow"];
     var gamePattern = [];
     var userClickedPattern = [];
-    var gameRunning = false;
+    var start = false;
     var level = 0;
 
-    
-    // Select a random button
-    function nextSequence() {
+    // Start the game
+    $(document).keydown(function() {
+        if (!start){
+            
+            // Change "Press a Key to Start" to the current level 0
+            $("#level-title").text("Level "+ level);
 
+            nextSequence();
+            start = true;
+        }
+    })
+
+    // Select the random sequence
+    function nextSequence() {
         // Increase the level
         level++;
 
-        // Change the h1 to the currant level
-        $("h1").text("Level "+ level);
+        // Change the h1 to the current level
+        $("#level-title").text("Level "+ level);
 
-
+        // Returns a number between 0 and 3
         var random = Math.floor(Math.random() * 4);
 
-        // Pick a random colour
+        // Pick a random colour from the buttonColours array
         var randomChoosenColour = buttonColours[random];
 
         // Append the random button to the gamePattern array
         gamePattern.push(randomChoosenColour);
 
         // Flashes the random button
-        $("#"+randomChoosenColour).fadeOut(100).fadeIn(100).fadeIn(100);
+        var flashButton = "#"+randomChoosenColour;
+        $(flashButton).fadeOut(100).fadeIn(100).fadeIn(100);
 
         // Play the sound to the random button
         playSound(randomChoosenColour);
-
-        
-
-        
-
+        return false;
     }
     
     
     // Store in the array the button clicked by the user
     $("div").click(function(){
-        var userChoosenColour = this.id;
+        var userChoosenColour = $(this).attr("id");
         userClickedPattern.push(userChoosenColour);
 
         playSound(userChoosenColour);
         animatePress(userChoosenColour);
+        checkAnswer(userClickedPattern[userClickedPattern.length -1]);
     })
 
     // Plays the right song for each colour
     function playSound(colour) {
         var sound = new Audio("sounds/"+colour+".mp3");
         sound.play();
+        return false;
     }
 
     // Animates when the user clicks the button
@@ -59,19 +68,18 @@ $(document).ready(function() {
         var colour = "#"+colour
         $(colour).addClass("pressed");
         setTimeout(function(){$(colour).removeClass("pressed")}, 100)
+        return false;
     }
 
-    // Start the game
-    $(document).keydown(function() {
-        if (!gameRunning){
-            
-            // Change "Press a Key to Start" to the current level 0
-            $("h1").text("Level "+ level);
+    // Check the user's answer
+    function checkAnswer(currentLevel) {
 
-            nextSequence();
-            gameRunning = true;
-        }
-    })
+        if (currentLevel === gamePattern[gamePattern.length-1]){
+            console.log("right")
+        } else {console.log("wrong")}
+
+        return false;
+    }
 
     
 });
